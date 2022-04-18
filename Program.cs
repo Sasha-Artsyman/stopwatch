8f7e27704bea05efdef717d17b194325104b3790
Sasha Artsyman <65505084+Sasha-Artsyman@users.noreply.github.com>
2022-04-18 19:51:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string []args)
+    {
+        Console.Write("Input number of dices: ");
+        int numberOfDices = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the number of faces: ");
+        int numberOfFaces = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("--------------------");
+
+        Random rnd = new Random();
+        int total = 0;
+
+        for (int i = 1; i <= numberOfDices;)
+        {
+            int dice = rnd.Next(0, numberOfFaces);
+            if (dice > 0)
+            {
+                Console.WriteLine("Dice " + i + ": " + dice);
+                i++;
+                total += dice;
+            }
+        }
+
+        Console.WriteLine("--------------------");
+        Console.WriteLine("Total: " + total);
+
+        Console.ReadLine();
+    }
+}
